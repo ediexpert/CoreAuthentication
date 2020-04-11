855699ed2beee903adc84b776224a682f9e1f5da
Imran Bajwa <imranbajwa01@gmail.com>
2020-04-12 00:09:34 +0200

    custom policy and role authoirization

--- AuthWithPolicy/AuthRequirements/CustomRequireClaim.cs
+++ AuthWithPolicy/AuthRequirements/CustomRequireClaim.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
 namespace AuthWithPolicy.AuthRequirements
 {
     public class CustomRequireClaim: IAuthorizationRequirement
     {
-        
+        public CustomRequireClaim(string claimType)
+        {
+            ClaimType = claimType;
+        }
+
+        public string ClaimType { get; private set; }
+    }
+    public class CustomRequireClaimHandler : AuthorizationHandler<CustomRequireClaim>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomRequireClaim requirement)
+        {
+            var hasClaim = context.User.Claims.Any(x => x.Type == requirement.ClaimType);
+            if (hasClaim)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
     }
 }
